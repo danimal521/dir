f737f1d404e0b5586883b6126fcad35ac5e8dcf0
Dan Biscup <dabiscup@microsoft.com>
2020-07-08 00:45:40 -0500

    initial

---
+++ Backend2/Backend/Backend/DAL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Models;
+
+namespace Backend
+{
+    public class DAL
+    {
+        public const string SQL_CONNECTION                              = "connectionstring";
+        private static SqlConnection m_sqlClient                        = new SqlConnection(DAL.SQL_CONNECTION);
+
+
+        public static Models.Employee GetEmployeeByID(string EmployeeID)
+        {
+            Models.Employee rEmployee                                   = new Models.Employee();
+            try
+            {
+                m_sqlClient.Open();
+
+                using (SqlCommand cmd                                   = new SqlCommand("SELECT top 1 * FROM company", m_sqlClient))
+                {
+                    SqlDataAdapter da                                   = new SqlDataAdapter(cmd);
+                    DataSet ds                                          = new DataSet();
+                    da.Fill(ds);
+
+                    foreach (DataRow dr in ds.Tables[0].Rows)
+                    {
+                        rEmployee.FirstName                             = dr[1].ToString();
+                        rEmployee.LastName                              = dr[2].ToString();
+                        break;                        
+                    }
+                }
+
+                m_sqlClient.Close();
+            }
+            catch (Exception exError) { }
+
+            return rEmployee;
+        }    
+        
+
+    }
+}
